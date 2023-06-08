e3eee7e64ad2d066bc13c4043898547e8d2cdecb
Saihaj Gulati <saihaj.gulati@gmail.com>
2023-06-07 21:42:58 -0700

    Create LongestSubstringWithoutRepeatingChars.py

---
+++ SlidingWindow/LongestUniqueCharSubstring/LongestSubstringWithoutRepeatingChars.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if (len(s) == 0):
+            return 0
+
+        left = 0
+        right  = 1
+        currLetters = set()
+        currLetters.add(s[left])
+        maxLength = 1
+
+        while right < len(s):
+            ##get a duplicate letter, change window
+            if (s[right] in currLetters):
+                ##really important here to remove the left value and NOT the duplicate one because this way you're acctually changing the window
+                currLetters.remove(s[left])
+                left += 1
+            ## is new letter, so add to curr String and compare
+            else: 
+                currLetters.add(s[right])
+                maxLength = max(len(currLetters), maxLength)
+                right += 1
+        return maxLength

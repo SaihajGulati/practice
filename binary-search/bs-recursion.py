b701b7e3346be55a4d1556a8bd2d09803cc41e1e
Saihaj Gulati <saihaj.gulati@gmail.com>
2023-06-10 21:55:33 -0700

    Update bs-recursion.py

--- binary-search/bs-recursion.py
+++ binary-search/bs-recursion.py
@@ -9,8 +9,11 @@ class Solution(object):
         def binarySearch(l, r):
             if l > r:
                 return -1
-            ##python divides with decimials by default BUT converts it to int where needed
-            ##so technically need this if want int but don't need because when use m to access array, converted to int
+             ##python divides with decimials by default BUT converts it to int where needed
+             ##so technically need this if want int but don't need because when use m to access array, converted to int
+             ## could also do floor division
+             ## also watch out for overflow, a trick is 
+             ## m = l + ((r - l) // 2)
             m = int((l+r)/2)
             if nums[m] > target: 
                 r = m - 1

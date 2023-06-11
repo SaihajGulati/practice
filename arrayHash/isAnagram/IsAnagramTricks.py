464d987be4b3399ba31a0b10dc8dac6c8f960636
Saihaj Gulati <saihaj.gulati@gmail.com>
2023-06-11 14:52:18 -0700

    Update IsAnagramTricks.py

--- arrayHash/isAnagram/IsAnagramTricks.py
+++ arrayHash/isAnagram/IsAnagramTricks.py
@@ -15,7 +15,7 @@ class Solution(object):
         if len(s) != len(t):
             return False
         dictS, dictT = {}, {}
-        #goes through s and then if the letter is in dict, then adds 1 to count, otherwise, creates new entry
+        #goes through s and then if the letter is in dict, then adds 1 to count, otherwise, adds 1 to new entry starting with vaue 0
         for i in range(len(s)):
             dictS[s[i]] = 1 + dictS.get(s[i], 0)
             dictT[t[i]] = 1 + dictT.get(t[i], 0)

b1a8716f267df9f4c6c8a2acfaefa918270326b6
Saihaj Gulati <saihaj.gulati@gmail.com>
2023-06-23 10:48:56 -0700

    Update unique-paths-simplified.py

--- dp2d/unique-paths-simplified.py
+++ dp2d/unique-paths-simplified.py
@@ -5,7 +5,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        #FinalSolution -- T: O(nm) M: O(n)
         #need to set down here because will changed always but starting here
         #by setting to n vs n-1, save cases where n-1 = 0 and have to add if statements that slow code
         #also in big scheme of things, n vs n-1 size down vs two if statements chekcs at the start evens out tbh

8f5eb29f47a8e183ada5bdc22b3ab74dc1716c2b
Saihaj Gulati <saihaj.gulati@gmail.com>
2023-07-01 20:02:49 -0700

    Create diameter.universal.py

---
+++ trees/diameter.universal.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        #in python arrays are automatically accessible nonlocally by nested function so wouldn't even have to pass like this
+        def dfs(curr, maxLength):
+            #gotta do a while loop
+            if not curr:
+                #-2 so that when get past end and add 2, get to 0 for length at that spot
+                return 0
+
+            #get max length going down on each side
+            left = dfs(curr.left, maxLength)
+            right = dfs(curr.right, maxLength)
+
+            #left + right is diameter because height on each side includes between curr.left and curr and bw curr.right and curr
+            maxLength[0] = max(left + right, maxLength[0])
+
+            #because need to build up to calculate max height at each spot
+            return 1 + max(left, right)
+          
+        #would be possibly a reference in another language or could do with an array like this
+        maxL = [0]
+        dfs(root, maxL)
+        return maxL[0]

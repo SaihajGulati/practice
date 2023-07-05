7a737f65340f5edee758c48324f48f4a910bd270
Saihaj Gulati <saihaj.gulati@gmail.com>
2023-07-04 18:35:51 -0700

    Update k-closest-BEST.py

--- heap/k-closest-BEST.py
+++ heap/k-closest-BEST.py
@@ -9,6 +9,7 @@ class Solution:
             #avoid doing sqrt as is unnecessary because we don't need actual distance, 
             #just what is bigger/smaller which can be checked without sqrt being there as it won't change relative largeness
             #is really distance formula but since is from 0, the 0s are invisible and also not sqrting to save time
+            #but dangerous game bc still have to square each to get correct differences so if ever a bit confused on whether can, just be safe
             dist = x**2 + y**2
             minHeap.append([dist, x, y])
 

8167715efe27d0b4402453b7d216d3c356a22369
Saihaj Gulati <saihaj.gulati@gmail.com>
2023-07-09 17:00:27 -0700

    Update mincost-stairs-neet.py

--- dp1d/mincost-stairs-neet.py
+++ dp1d/mincost-stairs-neet.py
@@ -3,6 +3,7 @@ def minCostClimbingStairs(self, cost: List[int]) -> int:
       #saves some code in for loop and before, but makes for loop condition wack
       #this means goes from 3rd to last index to 0th since -1 is exclusive, and subtracts 1 each time
       #Final Solution -- T: O(n) M: O(1) bc use memory already given so memory complexity means extra memory complexity which here is constant
+      #but in interview have to ask if can modify original list
       for i in range(len(cost) - 3, -1, -1):
             cost[i] += min(cost[i + 1], cost[i + 2])
 

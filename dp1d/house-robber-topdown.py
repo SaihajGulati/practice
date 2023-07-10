ee207e10a4a1fffdeeafa3785c2e61d2d8c88a18
Saihaj Gulati <saihaj.gulati@gmail.com>
2023-07-09 17:05:51 -0700

    Create house-robber-topdown.py

---
+++ dp1d/house-robber-topdown.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+      #top-down
+        rob1 = 0
+        rob2 = 0
+
+      # T: O(n) M O(1)
+        for i in nums:
+            temp = max(i + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+
+      #here since maxing each time, just return last one
+        return rob2

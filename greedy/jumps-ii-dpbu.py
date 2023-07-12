f16c1a875847dbf480fa6cfbb640e247d13c68e2
Saihaj Gulati <saihaj.gulati@gmail.com>
2023-07-12 00:00:40 -0700

    Update jumps-ii-dpbu.py

--- greedy/jumps-ii-dpbu.py
+++ greedy/jumps-ii-dpbu.py
@@ -1,14 +1,16 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        #bottom-up dp --> in between solution
+        #bottom-up dp --> in between solution, og solution i got, fun but not optimal optimal
         #T: O(n^2) bc have to do n for outer for loop and inner does max n each time if jumps was big enough
-        #Extra M: O(1) but requires writing over given list
+        #Extra M: O(1) but requires writing over given list (ask in interview if can)
         for i in reversed(range(len(nums) - 1)):
             if i + nums[i] >= len(nums) - 1:
                 nums[i] = 1
             else:
-              #is max possible so that anything overrides it
-                minJumps = float(inf)
+                #need max number right now so min automatically replaces each time even if size of array is bigger than max value (1001)
+                #which could lead to number of jumps bigger than 1001 and don't want 1001 becoming min
+                #good to be safe and just use inf for this situation
+                minJumps = float("inf")
                 #min is important bc can't go off end
                 for j in range(i + 1, min((i + nums[i] + 1), len(nums) - 1)):
                     #have to add 1 because not using 0 here

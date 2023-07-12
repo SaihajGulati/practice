b15eecbf6b6ea39ef735e00e59b37e8dd0b542f7
Saihaj Gulati <saihaj.gulati@gmail.com>
2023-07-12 00:03:04 -0700

    Update jumps-ii-dptd.py

--- greedy/jumps-ii-dptd.py
+++ greedy/jumps-ii-dptd.py
@@ -3,7 +3,6 @@ class Solution:
         #top-down (moving foward) dp, WORST bc of memory issue here bc rewriting front
         # T: O(n^2) bc for each i (no total), you go jumps number of times which can be at worst all of n
         # Extra M: O(N) bc extra array needed
-        #on leetcode, didn't even pass bc of time/memory limit, proof of top-down not always better
 
        #need max number right now so min automatically replaces each time even if size of array is bigger than max value (1001)
        #which could lead to number of jumps bigger than 1001 and don't want 1001 becoming min

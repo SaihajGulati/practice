754d9150ea0fc5f05e823f2c2af7664033e2be0c
Saihaj Gulati <saihaj.gulati@gmail.com>
2023-07-12 19:20:42 -0700

    Update insert-simplified.py

--- intervals/insert-simplified.py
+++ intervals/insert-simplified.py
@@ -6,18 +6,18 @@ class Solution:
         #T: O(n) M: O(n)
         #could do diff ways with split up looping to append at end and start, but ends up being more complex for same runtime bc deep copy requires looping over all anyways, just less code
         for i in range(len(intervals)):
-            #if the end of the interval checking is less than new one's start, add to result because is cleanly before
+            #if the end of the interval i is less than new one's start, add to result because i interval is cleanly before
             if (intervals[i][1] < newInterval[0]):
                 result.append(intervals[i])
-            #otherwise if the start of the interval is after the end of the new one (so cleanly after)
+            #if that's the case and the start of i interval is after the end of the new interval's end (so i interval is cleanly after the new one
             elif (intervals[i][0] > newInterval[1]):
                 result.append(newInterval)
                 return result + intervals[i:]
-            #if get here, is not cleanly before or cleanly after
-            #can just do mins and maxes because already know is not clean
+            #if get here, i interval is not cleanly before or cleanly after
+            #can just do mins and maxes because already know is not clean so we are going to combine
             #setting 0th and 1th is better than doing newInterval = [min(intervals[i][0], newInterval[0]), max(newInterval[1], intervals[i][1])]
             #doing that means make new list each time which means it needs to allocate memory each time rather than constant time access
-            else:
+            else: #make new interval equal to bigger one and then checks of next time can help use find out 
                 newInterval[0] = min(intervals[i][0], newInterval[0])
                 newInterval[1] = max(newInterval[1], intervals[i][1])
         

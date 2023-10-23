3351b7c4ab1c4c208a220801958dbf3b0ff682d3
Saihaj Gulati <saihaj.gulati@gmail.com>
2023-10-23 03:51:28 -0700

    Create max_window_neet.py

---
+++ sliding-window/max_window_neet.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        q = collections.deque()  # index
+        #ok to have this slightly unnecesary l that requires a check, because already need a check for output array appending
+        l = r = 0
+        
+      # T:O(n) M:O(n)
+        while r < len(nums):
+            # pop smaller values from q
+            #have to do q check first
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            
+          q.append(r)
+
+            # remove left val from window
+            #since appended before this, don't have to do q check here --> SMART  
+            if l > q[0]:
+                q.popleft()
+      
+            #making sure right index is big enough that at least a k window has been made (bc at start will have max without valid window)
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+
+        return output

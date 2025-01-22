419e69ca3333dd1641d1394241021159bc6dabc3
Saihaj Gulati <saihaj.gulati@gmail.com>
2025-01-22 08:54:46 -0800

    Create sort-power-tuple.py

---
+++ array-hash/sort-power-tuple.py
@@ -0,0 +1,22 @@
+class Solution:
+    #T: O(hi-lo * max power of x --> 2^31 + nlogn for sorting)
+    
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        stepList = []
+        for i in range(lo, hi + 1):
+            x = i
+            steps = 0
+            while x != 1:
+                steps += 1
+                if x % 2 == 0:
+                    x /= 2
+                else:
+                    x = 3 * x + 1
+            stepList.append((i, steps))
+        
+        sortedList = sorted(stepList, key = operator.itemgetter(1, 0))
+
+        return sortedList[k-1][0]
+            
+
+        
